e9024f9e51a49f042dd57bb250f41664f942b4b2
blytherk <blytherk@gmail.com>
2015-12-02 19:08:38 -0900

    will this work?
    we may have a winner!

---
+++ GroupIdentityProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GroupIdentityProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GroupIdentityProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GroupIdentityProject.Global" Language="C#" %>
